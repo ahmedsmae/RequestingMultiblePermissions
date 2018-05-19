b44126c435126ac59d88e405602e96ff3c266e86
Ahmed Afifi <ahmed.mohsem.afifi@gmail.com>
2018-05-19 07:44:59 +0400

    Basic coding

--- app/src/main/java/com/ahmed_smae/requestingmultiblepermissions/Permissions.java
+++ app/src/main/java/com/ahmed_smae/requestingmultiblepermissions/Permissions.java
@@ -7,27 +7,25 @@ import android.Manifest;
  */
 public class Permissions {
     public static String[] permissions = new String[]{
+            "",
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
+            //Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
+            //Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.READ_CONTACTS,
-            Manifest.permission.WRITE_CONTACTS,
+            //Manifest.permission.WRITE_CONTACTS,
             Manifest.permission.CALL_PHONE
     };
 
-    public static Integer[] requestCodes = new Integer[]{
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8
-    };
+    public static final int WRITE_EXTERNAL_STORAGE = 1;
+    //public static final int READ_EXTERNAL_STORAGE = 2;
+    public static final int CAMERA = 3;
+    public static final int ACCESS_COARSE_LOCATION = 4;
+    //public static final int ACCESS_FINE_LOCATION = 5;
+    public static final int READ_CONTACTS = 6;
+    //public static final int WRITE_CONTACTS = 7;
+    public static final int CALL_PHONE = 8;
 
 
 }
